57de8774f10b20313cc110c28bf4e3312d49e7cd
amiru20060110-bit <amiru20060110@gmail.com>
2026-08-13 22:15:21 +0530

    Enhance Player struct with rent and loan attributes
    Added new fields to Player struct for rent and loan management.

--- players.h
+++ players.h
@@ -7,6 +7,15 @@ typedef struct {
     char name[50];
     int balance;
     int position;
+    int rent_multiplier; // Multiplier for rent due to event effects
+    int rent_boosts_rounds_remaining; // Number of turns the rent boost effect remains active
+    float market_rise_multiplier; // Multiplier for property value due to event effects
+    float market_downturn_multiplier; // Multiplier for property value due to event effects
+    int active_loan; //loan state
+    int loan_size; //loan size
+    int loan_rounds_remaining; //check available rounds for loan ending
+    
+
 } Player;
 
 #endif // PLAYERS_H
